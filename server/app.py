51bdd7013e1c3bf0793f71ffd0fea850bedb2c9b
Duke <dukemorise33@gmail.com>
2024-02-04 20:23:25 -0800

    completed lesson

--- server/app.py
+++ server/app.py
@@ -47,10 +47,50 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource):
+    
+    def post(self):
+        #Whatever the user inputs will be through a request... Big FYI
+        #This is pretty much filtering the Users in the User class to get the username that the client inputted in their request
+        user = User.query.filter(User.username == request.get_json()["username"]).first()
+
+        #adds the user's id in the session as user_id
+        #the user information will be stored in the session (browser)
+        session['user_id'] = user.id
+
+        #returns the actual user to the response.
+        #This step is separate from adding things to the session.
+        response = make_response(user.to_dict(), 200)
+
+        return response
+    
+class Logout(Resource):
+
+    def delete(self):
+        #sets the session's user_id
+        session['user_id'] = None
+        return make_response({'message': '204: No Content'}, 204)
+    
+class CheckSession(Resource):
+
+    def get(self):
+        user = User.query.filter(User.id == session.get("user_id")).first()
+        if user:
+            return make_response(jsonify(user.to_dict()), 200)
+        else:
+            return make_response({}, 401)
+
+
+
+
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
